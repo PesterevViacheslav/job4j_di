36a29647547f4729eb9d2574ab3ae3f10b4ef0d3
vpesterev <v.pesterev@m-developer.ru>
2023-07-22 15:41:11 +0300

    Решение задач уровня Middle. Категория : 3.5. Spring. Тема : 3.4.1. DI. Внедрение зависимостей 3. @Autowired. Внедрение зависимости [#261276]

--- src/main/java/ru/job4j/di/StartUI.java
+++ src/main/java/ru/job4j/di/StartUI.java
@@ -1,4 +1,5 @@
 package ru.job4j.di;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 /**
  * Class StartUI - Запуск приложения.
@@ -10,14 +11,10 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class StartUI {
-
+    @Autowired
     private Store store;
+    @Autowired
     private ConsoleInput consoleInput;
-
-    public StartUI(Store store, ConsoleInput consoleInput) {
-        this.store = store;
-        this.consoleInput = consoleInput;
-    }
 
     public void add(String value) {
         store.add(value);
